8c3f10fc975d5d111ac869ec1ff32f85456d1c3b
AlexPolotnyanko <Shadow_A97@mail.ru>
2016-12-16 19:44:45 +0300

    Add files via upload

---
+++ dialog.h
@@ -0,0 +1,27 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QString>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private:
+    Ui::Dialog *ui;
+signals:
+    void FilePath(const QString &pathStr);
+private slots:
+    void okClicked();
+};
+
+#endif // DIALOG_H
